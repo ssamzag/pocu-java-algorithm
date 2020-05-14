39442b32d32c1f2aa3cc766ce55f6bb620f3da9b
ssamzag <ssamzag@gmail.com>
2020-05-14 11:10:22 +0900

    no message

--- Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
+++ Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
@@ -3,6 +3,7 @@ package academy.pocu.comp2500.lab2;
 public class ComplexNumber {
     public double real;
     public double imaginary;
+    private final int IMAGINARY_SQUARED = -1;
 
     public ComplexNumber() {
         this.real = 0.0;
@@ -20,31 +21,39 @@ public class ComplexNumber {
     }
 
     public boolean isReal() {
-
-        return true;
+        return this.real != 0.0 && this.imaginary == 0.0;
     }
 
     public boolean isImaginary() {
-
-        return true;
+        return real == 0.0 && this.imaginary != 0.0;
     }
 
-    public ComplexNumber add() {
-
-        return new ComplexNumber(0,0);
+    public ComplexNumber getConjugate() {
+        return new ComplexNumber(this.real, this.imaginary * -1);
     }
 
-    public ComplexNumber subtract() {
-
-        return new ComplexNumber(0,0);
+    public ComplexNumber add(ComplexNumber complexNumber) {
+        return new ComplexNumber(this.real + complexNumber.real,this.imaginary + complexNumber.imaginary);
     }
 
-    public ComplexNumber multiply() {
+    public ComplexNumber subtract(ComplexNumber complexNumber) {
 
-        return new ComplexNumber(0,0);
+        return new ComplexNumber(this.real - complexNumber.real,this.imaginary - complexNumber.imaginary);
     }
 
-    public ComplexNumber divide() {
+    public ComplexNumber multiply(ComplexNumber complexNumber) {
+        if (this.imaginary == 0 && complexNumber.imaginary == 0) {
+            return new ComplexNumber(this.real * complexNumber.real, 0);
+        }
+        double real = this.real * complexNumber.real - this.imaginary * complexNumber.imaginary;
+        double imaginary = this.real * complexNumber.imaginary + this.imaginary * complexNumber.real;
+        return new ComplexNumber(real,imaginary);
+    }
+
+    public ComplexNumber divide(ComplexNumber complexNumber) {
+        if (complexNumber.real == 0 && complexNumber.imaginary == 0) {
+            return new ComplexNumber(0,0);
+        }
 
         return new ComplexNumber(0,0);
     }
