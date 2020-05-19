72dc5dd6036b66a0a13febe909026fa4b90cd422
ssamzag <ssamzag@gmail.com>
2020-05-19 16:44:24 +0900

    no message

--- Lab3/src/academy/pocu/comp2500/lab3/ListItem.java
+++ Lab3/src/academy/pocu/comp2500/lab3/ListItem.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 
 public class ListItem {
     private String text;
-    private char bulletStyle;
+    private final char bulletStyle;
     private int depth;
-    private final ArrayList<ListItem> listItem;
+    private final ArrayList<ListItem> listItemArrayList;
 
     public ListItem(String text) {
         this(text, '*');
@@ -16,7 +16,7 @@ public class ListItem {
         this.text = text;
         this.bulletStyle = bulletStyle;
         this.depth = 0;
-        this.listItem = new ArrayList<ListItem>();
+        this.listItemArrayList = new ArrayList<ListItem>();
     }
 
     public String getText() {
@@ -32,31 +32,29 @@ public class ListItem {
     }
 
     public void addSublistItem(ListItem listItem) {
-        this.listItem.add(listItem);
+        this.listItemArrayList.add(listItem);
     }
+
     public ListItem getSublistItem(int index) {
-        return this.listItem.get(index);
+        return this.listItemArrayList.get(index);
     }
 
     public void removeSublistItem(int index) {
-        this.listItem.remove(index);
+        this.listItemArrayList.remove(index);
     }
 
-    private ListItem setDepth(int depth) {
-        this.depth = depth == 2 ? depth : ++depth;
-        return this;
-    }
-
-    private String getSpacingBullet() {
-        return " ".repeat(this.depth * 4) + this.bulletStyle + " ";
+    private void setDepth(int depth) {
+        this.depth = depth > 2 ? 2 : depth;
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(String.format(getSpacingBullet() + this.getText() + "%s", System.lineSeparator()));
-        for (var list: listItem) {
-            sb.append(list);
+        sb.append(String.format(" ".repeat(4 * this.depth) + this.bulletStyle + " " + this.getText() + "%s", System.lineSeparator()));
+
+        for (var listItem: listItemArrayList) {
+            listItem.setDepth(this.depth + 1);
+            sb.append(listItem);
         }
         return sb.toString();
     }
