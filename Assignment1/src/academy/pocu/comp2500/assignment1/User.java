794fbf1b936d1e40e695aa354482b612b65d73a5
ssamzag <ssamzag@gmail.com>
2020-05-22 16:46:42 +0900

    no message

--- Assignment1/src/academy/pocu/comp2500/assignment1/User.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/User.java
@@ -2,28 +2,23 @@ package academy.pocu.comp2500.assignment1;
 
 public class User {
     private final String userId;
-    private final String userName;
     private final String nickName;
     private final String displayName;
     private String emailAddress;
 
-    public User(String userId, String userName, String nickName) {
+    public User(String userId, String nickName) {
         this.userId = userId;
-        this.userName = userName;
         this.nickName = nickName;
         this.displayName = nickName;
     }
 
-    public User(String userId, String userName, String nickName, String emailAddress) {
-        this(userId, userName, nickName);
+    public User(String userId, String nickName, String emailAddress) {
+        this(userId, nickName);
         this.emailAddress = emailAddress;
     }
 
     public String getUserId() {
         return this.userId;
-    }
-    public String getUserName() {
-        return this.userName;
     }
 
     public String getNickName() {
@@ -32,5 +27,9 @@ public class User {
 
     public String getDisplayName() {
         return this.displayName;
+    }
+
+    public String getEmailAddress() {
+        return this.emailAddress;
     }
 }
