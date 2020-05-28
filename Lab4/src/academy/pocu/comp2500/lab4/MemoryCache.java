988d88b10504fc7abdf6cde3e163cdb30a5e7de0
ssamzag <ssamzag@gmail.com>
2020-05-28 23:40:18 +0900

    no message

--- Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
+++ Lab4/src/academy/pocu/comp2500/lab4/MemoryCache.java
@@ -1,88 +1,128 @@
 package academy.pocu.comp2500.lab4;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.stream.IntStream;
 
 public class MemoryCache {
     private static ArrayList<MemoryCache> memoryCache;
     private static int maxMemoryCacheCount;
     private int maxEntryCount;
-    private String instance;
+    private String data;
     private LinkedHashMap<String, String> entry;
-    private static long cacheMadeSeq;
-    private long seq;
     private EvictionPolicy entryEvictionPolicy;
+    private LinkedHashSet<String> entryMetaData;
 
     public MemoryCache(String data) {
-        this.instance = data;
-        this.seq = ++cacheMadeSeq;
-        maxMemoryCacheCount = Integer.MAX_VALUE;
-    }
-
-    public MemoryCache getInstance(String data) {
-        if (maxMemoryCacheCount <= memoryCache.size()) {
-            MemoryCache leastRecentlyCache = memoryCache
-                    .stream()
-                    .min(Comparator.comparing(MemoryCache::getSeq))
-                    .get();
-            memoryCache.remove(leastRecentlyCache);
-        }
-        memoryCache.add(new MemoryCache(data));
-        this.entry = new LinkedHashMap<String, String>(22);
+        this.data = data;
+        this.entry = new LinkedHashMap<String, String>(16, 0.75f, true);
+        this.entryMetaData = new LinkedHashSet<String>();
+        this.maxEntryCount = Integer.MAX_VALUE;
         this.entryEvictionPolicy = EvictionPolicy.LEAST_RECENTLY_USED;
-        return memoryCache.get(memoryCache.size() - 1);
-
     }
 
-    public long getSeq() {
-        return this.seq;
+    public static MemoryCache getInstance(String data) {
+        if (memoryCache == null) {
+            memoryCache = new ArrayList<MemoryCache>();
+            maxMemoryCacheCount = maxMemoryCacheCount == 0 ? Integer.MAX_VALUE : maxMemoryCacheCount;
+        }
+        for (MemoryCache cache : memoryCache) {
+            if (cache.data.equals(data)) {
+                memoryCache.remove(cache);
+                memoryCache.add(cache);
+                return cache;
+            }
+        }
+        if (maxMemoryCacheCount == memoryCache.size()) {
+            memoryCache.remove(0);
+        }
+
+        var cache = new MemoryCache(data);
+        memoryCache.add(cache);
+        return cache;
     }
 
     public void clear() {
         memoryCache.clear();
     }
 
-    public void setMaxInstanceCount(int count) {
+    public static void setMaxInstanceCount(int count) {
         maxMemoryCacheCount = count;
+        if (count < memoryCache.size()) {
+            IntStream.range(0,  memoryCache.size() - count).forEach(i -> memoryCache.remove(i));
+        }
     }
 
     public void setEvictionPolicy(EvictionPolicy policy) {
         this.entryEvictionPolicy = policy;
     }
 
+    public void setMaxEntryCount(int maxEntryCount) {
+        if (this.entry.size() > maxEntryCount) {
+            var size = entry.size() - maxEntryCount;
+            for (int i = 0; i < size; ++i){
+                runEntryEvictionPolicy();
+            }
+
+        }
+        this.maxEntryCount = maxEntryCount;
+    }
+
     public void addEntry(String key, String value) {
+        changeMemoryCachePriority();
         if (entry.containsKey(key)) {
             this.entry.put(key, value);
             return;
         }
+        if (this.entry.size() == maxEntryCount) {
+            runEntryEvictionPolicy();
+        }
 
-        runEntryEvictionPolicy();
+        this.entryMetaData.add(key);
         this.entry.put(key, value);
     }
 
-    public LinkedHashMap<String, String> getEntryOrNull() {
-        return this.entry.size() == 0 ? null : this.entry;
+    public String getEntryOrNull(String key) {
+        changeMemoryCachePriority();
+        return this.entry.get(key);
+    }
+
+    private void changeMemoryCachePriority() {
+        memoryCache.remove(this);
+        memoryCache.add(this);
     }
 
     private void runEntryEvictionPolicy() {
-        if (this.maxEntryCount < this.entry.size()) {
-            switch (this.entryEvictionPolicy) {
-                case FIRST_IN_FIRST_OUT:
-                    this.entry.remove(0);
-                    break;
-                case LAST_IN_FIRST_OUT:
-                    this.entry.remove(this.entry.size() - 1);
-                    break;
-                case LEAST_RECENTLY_USED:
-
-                    break;
-                default:
-                    assert (false);
-            }
+        var metaIterator = this.entryMetaData.iterator();
+        switch (this.entryEvictionPolicy) {
+            case FIRST_IN_FIRST_OUT:
+                if (metaIterator.hasNext()) {
+                    this.entry.remove(metaIterator.next());
+                    metaIterator.remove();
+                }
+                break;
+            case LAST_IN_FIRST_OUT:
+                if (this.entryMetaData.size() > 0) {
+                    String lastKey = "";
+                    while (metaIterator.hasNext()) {
+                        lastKey = metaIterator.next();
+                    }
+                    this.entry.remove(lastKey);
+                    metaIterator.remove();
+                }
+                break;
+            case LEAST_RECENTLY_USED:
+                var entryIterator = this.entry.entrySet().iterator();
+                if (entryIterator.hasNext()) {
+                    var key = entryIterator.next().getKey();
+                    this.entryMetaData.remove(key);
+                    entryIterator.remove();
+                }
+                break;
+            default:
+                assert (false);
         }
-
     }
-
 }
